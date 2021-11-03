f3d6a6f130b192bb01fd87420510722fc1ae1870
sebfie <sebastien.fieloux@gmail.com>
2021-11-03 16:16:40 +0100

    Use 1.0.10 version

--- cocolis/vendor/composer/installed.php
+++ cocolis/vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.9',
-        'version' => '1.0.9.0',
+        'pretty_version' => '1.0.10',
+        'version' => '1.0.10.0',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../../',
         'aliases' => array(),
@@ -22,8 +22,8 @@
             'dev_requirement' => false,
         ),
         'cocolis/woocommerce' => array(
-            'pretty_version' => '1.0.9',
-            'version' => '1.0.9.0',
+            'pretty_version' => '1.0.10',
+            'version' => '1.0.10.0',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../../',
             'aliases' => array(),
